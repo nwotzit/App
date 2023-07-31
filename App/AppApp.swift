03f58f2b77c47b6e44b9187591c42eca5e8dc470
Nick <namrathawarrier@gmail.com>
2023-07-31 22:40:55 +0100

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Veena on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
