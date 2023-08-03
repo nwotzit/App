b829a4b9221dc12ae45ee9f54347a581b6d8304a
tue <tuethai007@gmail.com>
2023-08-03 11:08:31 -0700

    animation + debug

--- App/Meditation View/Home.swift
+++ App/Meditation View/Home.swift
@@ -1,27 +1,52 @@
-//
-//  Home.swift
-//  App
-//
-//  Created by tue on 8/2/23.
-//
-
 import SwiftUI
 
 struct Home: View {
     @State var currentType: Breath = sampleTypes[0]
     @Namespace var animation
-    @State var showBreatheView:Bool = false
+    @State var showBreatheView: Bool = false
     @State var startAnimation: Bool = false
+    @State var timerCount: CGFloat = 0
+    @State var breatheAction: String = "Inhale"
+    @State var count: Int = 0
     
     var body: some View {
         ZStack {
             Background()
             Content()
             
+            Text(breatheAction)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .top)
+                .padding(.top,50)
+                .opacity(showBreatheView ? 1 : 0)
+                .animation(.easeInOut(duration: 1), value: breatheAction)
+                .animation(.easeInOut(duration: 1), value: showBreatheView)
+            
         }
-    }
+        //TIMER
+        .onReceive(Timer.publish(every: 0.01, on: .main, in: .common) .autoconnect()) { _ in
+            if showBreatheView {
+                if timerCount >= 3.2 {
+                    timerCount = 0
+                    breatheAction = (breatheAction == "Exhale" ? "Inhale" : "Exhale")
+                    withAnimation(.easeInOut(duration: 3) .delay(0.1)) {
+                        self.startAnimation.toggle()
+                    }
+                    
+                }else {
+                    timerCount += 0.01
+                }
+                count = 3 - Int(timerCount)
+            } else {
+                timerCount = 0
+            }
+        }
+        
+    } //i think it's this one
+    //MAIN
     @ViewBuilder
-        .opacity(showBreatheView ? 0 : 1)
+    
     func Content()-> some View{
         VStack{
             HStack{
@@ -29,19 +54,9 @@ struct Home: View {
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
                     .frame(maxWidth:.infinity, alignment: .leading)
-            
+                
                     .opacity(showBreatheView ? 0 : 1 )
                 Button (action: startBreathing) {
-                    
-                } label: {
-                    Image(systemName: "suit.heart")
-                        .font(.title2)
-                        .foregroundColor(.white)
-                        .frame(width: 42, height: 42)
-                        .background{
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.ultraThinMaterial)
-                        }
                     
                 }
                 Button(action: startBreathing) {
@@ -51,15 +66,16 @@ struct Home: View {
                         .foregroundColor(.white)
                         .padding(.vertical, 15)
                         .frame(maxWidth: .infinity)
-                        .background {
-                            if showBreatheView {
-                                
-                            }else {
-                                RoundedRectangle(cornerRadius: 12, style: .continuous, .fill(.blue.gradient))
-                            }
-                            
-                               
-                        }
+                    //                        .background {
+                    //                            if showBreatheView {
+                    //                                RoundedRectangle(cornerRadius: 12, style: .continuous, .stroke(.white.opaciy(0.5)))
+                    //
+                    //                            }else {
+                    //                                RoundedRectangle(cornerRadius: 12, style: .continuous, .fill(.blue.gradient))
+                    //                            }
+                    //
+                    //
+                    //                        }
                 }
             }
             .padding()
@@ -73,48 +89,55 @@ struct Home: View {
                         .font(.title3)
                         .foregroundColor(.white)
                         .opacity(showBreatheView ? 0 : 1 )
-            
-                            
+                    
+                    
                 }
-               
+                
                 .frame(width: size.width, height: size.height, alignment: .bottom)
                 
             }
-            }
+        }
         
         .frame(maxHeight: .infinity, alignment: .top)
-        }
-        @ViewBuilder
+    }
+    @ViewBuilder
     func BreatheView (size: CGSize) -> some View {
         ZStack{
             
             ForEach(1...8, id: \.self){index in
-               Circle()
+                Circle()
                     .fill(.blue.gradient.opacity(0.5))
                     .frame(width: 150, height: 150)
-                    .offset(x:75)
+                    .offset(x: startAnimation ? 0 : 75)
                     .rotationEffect(.init(degrees: Double(index * 45)))
+                
                 
             }
         }
-        .padding()
+        .scaleEffect(startAnimation ? 0.8 : 1)
+        .overlay(content: {
+            Text("\(count == 0 ? 3 : count)")
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .animation(.easeInOut, value: count)
+                .opacity(showBreatheView ? 1 : 0)
+        })
+        .frame(height: (size.width - 40))
     }
-        
-    }
-    @ViewBuilder
+    
+}
+@ViewBuilder
 func Background()-> some View {
     Image("BG")
         .resizable(resizingMode: .stretch)
         .ignoresSafeArea()
 }
-        func startBreathing(){
-            withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)){
-                showBreatheView = true
-            }
-            
-        }
-    
-    
+
+func startBreathing() {
+    print("testing")
+}
+
 
 
 struct Home_Previews: PreviewProvider {
